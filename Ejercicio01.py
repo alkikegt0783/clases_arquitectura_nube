4b0bba6735f056a80857e3fbbe7eced831b2e210
Alcides Garcia <alkikegt@gmail.com>
2024-10-21 20:40:15 -0500

    version1

---
+++ Ejercicio01.py
@@ -0,0 +1,13 @@
+#Hola mundo desde Python
+print("Hola Mundo desde python") #Este es comentario
+
+"""
+Variables & tipo de datos
+"""
+
+nombre_persona="Juan Perez"
+edad_persona=40
+sueldo_persona=1700.90
+estado_civil=True
+
+print ("Nombre pesona: ",nombre_persona)
---
+++ ejemplo.py
@@ -0,0 +1,6 @@
+def saludar (nombre):
+ return f'Hola {nombre}'
+
+nombre_usuario=input("¿Cual es tu nombre?")
+saludo= saludar (nombre_usuario)
+print(saludo)
